d9ec89a5ad722f5d4f4bbb3bc7362423a76ef326
Stef <stef.robbe@gmail.com>
2022-12-04 16:11:08 +0100

    day 4, part 2

--- day4/main.go
+++ day4/main.go
@@ -12,22 +12,31 @@ type Section struct {
 	to   int
 }
 
+// https://github.com/tiri/aoc22/blob/main/04.go#L14
+func (p Section) len() int {
+	return p.to - p.from
+}
+
 func getPair(line string) (Section, Section) {
 	var firstElf, secondElf Section
 
 	// https://github.com/tiri/aoc22/blob/main/04.go#L25
 	fmt.Sscanf(line, "%d-%d,%d-%d", &firstElf.from, &firstElf.to, &secondElf.from, &secondElf.to)
 
+	if firstElf.len() < secondElf.len() {
+		tmp := firstElf
+		firstElf = secondElf
+		secondElf = tmp
+	}
+
 	return firstElf, secondElf
 }
 
 func comparePair(firstElf Section, secondElf Section) bool {
-	overlaps := false
-	if firstElf.from <= secondElf.from && firstElf.to >= secondElf.to {
-		overlaps = true
-	}
-	if secondElf.from <= firstElf.from && secondElf.to >= firstElf.to {
-		overlaps = true
+	overlaps := true
+
+	if firstElf.to < secondElf.from || secondElf.to < firstElf.from {
+		overlaps = false
 	}
 
 	return overlaps
